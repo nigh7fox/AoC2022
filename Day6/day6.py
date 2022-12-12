2c6e7fce2646493f90b6ff4d9023fdd4e5ad13fb
Joshua Tromp <joshua.tromp@nslogin.nl>
2022-12-12 12:58:55 +0100

    day 6 done

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,13 +1,20 @@
 def readInput():
-    with open('example_input.txt') as f:
+    with open('input.txt') as f:
         lines = f.readlines()
     linesFormatted = [x.strip('\n') for x in lines]
     return linesFormatted
 
-def startOfPacketMarket(input):
-    for dsBuffer in input:
-        print(dsBuffer)
-    return input
+def startOfPacketMarket(input, charSlice):
+    marker = 0
+    for idx, dsBuffer in enumerate(input):
+        for idxChar, bufferChar in enumerate(dsBuffer):
+            dsBufferSetLength = len(set(dsBuffer[idxChar:idxChar+charSlice])) # remove duplicates
+            if (dsBufferSetLength > (charSlice-1) and marker == 0): # 4 characters with 1 double = 3 unique characters
+                marker = idxChar + charSlice
+                print('Marker at position: %d' % (marker))
+        marker = 0
+    return marker
 
 if __name__ == "__main__":
-    startOfPacketMarket(readInput())
+    # v1 = startOfPacketMarket(readInput(), 4)
+    v2 = startOfPacketMarket(readInput(), 14)
